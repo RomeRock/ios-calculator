63f6b54b4f6fea4317bc1681c8f1b0f3c107722d
Miguel Serrato <jmserrato@narrowcastdigital.com>
2017-02-27 15:57:11 -0600

    Add New Design

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  iosCalculator
+//
+//  Created by NDM on 11/23/16.
+//  Copyright © 2016 Rome Rock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet var menuItem: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if self.revealViewController() != nil {
+            menuItem.target = self.revealViewController()
+            menuItem.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            self.revealViewController().panGestureRecognizer().delegate = self
+        }
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openButtonPressed(_ sender: Any) {
+        let calculatorViewController:UIViewController = UIViewController()
+        let customView = Bundle.main.loadNibNamed("CalculatorView", owner: nil, options: nil)?[0] as! CalculatorView
+        
+        calculatorViewController.view = customView
+        
+        customView.viewController = calculatorViewController
+        
+        calculatorViewController.modalPresentationStyle = .overFullScreen
+        calculatorViewController.modalTransitionStyle = .crossDissolve
+        present(calculatorViewController, animated: true, completion: nil)
+    }
+    
+
+}
+
